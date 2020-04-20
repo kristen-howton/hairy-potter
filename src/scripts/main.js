15c93ecd6a75ddfe5b4fe9b7efaeed2dfd044349
kristen-howton <kristen.howton@gmail.com>
2020-04-20 10:59:06 -0500

    make pottery function working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,19 @@
 // Imports go first
 
+import { makePottery } from "./PotteryWheel.js";
+
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let Kylix = makePottery("Kylix", 1, 2)
 
+let Pelike = makePottery("Pelike", 2, 2)
+
+let Olpe = makePottery("Olpe", 3, 3)
+
+let Oinochoe = makePottery("Oinochoe", 4, 4)
+
+let Bell_krater = makePottery("bell krater", 4, 4)
 
 // Fire each piece of pottery in the kiln
 
